8f59ab93d4c88776342af2a6d3affe9cf10a12d5
dihia-lyz <dihia.lyazid97@gmail.com>
2024-01-06 15:53:25 +0100

    update db credentials

--- meetPlanning/src/main/java/com/canalPlus/meetPlanning/MeetPlanningApplication.java
+++ meetPlanning/src/main/java/com/canalPlus/meetPlanning/MeetPlanningApplication.java
@@ -1,5 +1,6 @@
 package com.canalPlus.meetPlanning;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MeetPlanningApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(MeetPlanningApplication.class, args);
 	}
 
